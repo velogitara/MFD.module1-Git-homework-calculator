79a34734de1a5ba5f55a17181c4d6a35e7f8d826
Oleksandr Pavlishchev <velogitara@gmail.com>
2025-02-12 20:28:25 +0400

    feat(ui)/CommonLayout: удаляет иконку юзера в левом углу

--- src/components/CommonLayout/CommonLayout.tsx
+++ src/components/CommonLayout/CommonLayout.tsx
@@ -1,20 +1,15 @@
-import {FC, PropsWithChildren} from 'react';
-import {Avatar, Layout} from 'antd';
-import {UserOutlined} from '@ant-design/icons';
+import { FC, PropsWithChildren } from "react";
+import { Layout } from "antd";
+// import {UserOutlined} from '@ant-design/icons';
 
-
-export const CommonLayout: FC<PropsWithChildren> = ({
-  children
-}) => {
+export const CommonLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <Layout>
       <Layout.Header>
-        <Avatar shape="square" size={'small'} icon={<UserOutlined />} />
+        {/* <Avatar shape="square" size={'small'} icon={<UserOutlined />} /> */}
       </Layout.Header>
-      <Layout.Content>
-        {children}
-      </Layout.Content>
+      <Layout.Content>{children}</Layout.Content>
       <Layout.Footer> </Layout.Footer>
     </Layout>
-  )
-}
+  );
+};
